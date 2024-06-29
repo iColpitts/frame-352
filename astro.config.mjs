807d93413089b0a3ee3ceb805e3b9e52c3a056c7
Izzie <izzieccampbell@gmail.com>
2024-06-29 13:28:26 -0400

    Form working and ugly

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
 import sitemap from '@astrojs/sitemap';
+import vue from '@astrojs/vue';
+
 
 export default defineConfig({
   site: 'https://feature-tour.netlify.app',
   integrations: [
     preact(),
+    vue(),
     sitemap({
       canonicalURL: 'https://feature-tour.netlify.app'
     })
---
+++ src/components/sighting-form.vue
@@ -0,0 +1,137 @@
+<template>
+    <form
+      class="form"
+      method="POST"
+      name="sightings"
+      onchange="logSubmit()"
+      id="form"
+      netlify
+    >
+      <label for="name">Name</label>
+      <input type="text" id="name" name="name" />
+
+      <label for="conclusion">Conclusion</label>
+      <textarea name="conclusion"></textarea>
+
+    <div v-for="index in count" :key="index">
+        <section class="sighting" :id="`sighting-${ index }`">
+          <p>#{{ index }}</p>
+          <!-- <img id="blah" src="https://placehold.co/600x400/orange/white?text=upload+image" alt="your image" /> <br> -->
+          <input type="file" :id="`file_${ index }`" :name="`file_${ index }`"/><br>
+
+          <label :for="`date_${index}`">Date</label><br>
+          <input type="datetime-local" :id="`date-${index}`" :name="`date-${index}`"/><br>
+
+          <label :for="`notes_${index}`">Notes</label><br>
+          <textarea :name="`notes_${index}`"></textarea>
+        </section>
+    </div>
+  <input type="button" @click="addSighting" value="Add Sighting" />
+  <label><input type="checkbox" id="consent" name="consent"/> I consent to have my sightings published by Maxwell Lander.</label>
+      
+      <button type="submit" class="button">
+        Submit
+      </button>
+    </form>
+</template>
+
+<script>
+export default {
+  name: "SightingsForm",
+  data: () => ({
+      count: [1],
+    }),
+  // props: {
+  //   count: {
+  //     type: Array,
+  //     default: [1],
+  //   },
+  // },
+  methods: {
+    readURL() {
+        if (input.files && input.files[0]) {
+          var reader = new FileReader();
+
+          reader.onload = function (e) {
+              $('#blah')
+                  .attr('src', e.target.result);
+          };
+
+          reader.readAsDataURL(input.files[0]);
+      }
+    },
+    addSighting() {
+      console.log("add sighitng")
+      console.log(this.count)
+      this.count.push(this.count.length + 1)
+    },
+  }
+}
+</script>
+
+<style>
+.form {
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
+  width: 35ch;
+}
+
+.form label {
+  color: var(--heading);
+  font-size: 0.625rem;
+  font-weight: 600;
+  letter-spacing: 0.2em;
+  margin-top: 1rem;
+  text-transform: uppercase;
+}
+
+.form label:first-of-type {
+  margin-top: 0;
+}
+
+.form input,
+.form textarea {
+  color: var(--text);
+  font-family: inherit;
+  font-size: 1rem;
+  margin-top: 0.25rem;
+  padding: 0.25rem 0.5rem;
+}
+
+.form textarea {
+  height: 100px;
+}
+
+.button {
+  margin-top:1rem;
+  border:none;
+}
+
+
+.heading,
+.docs {
+  text-align: center;
+}
+
+.sighting {
+  border: 1px solid black;
+  padding: 0 1rem;
+}
+
+img{
+  max-width:250px;
+}
+</style>
+
+<!-- <script setup lang="ts">
+import { ref } from 'vue';
+
+const count = ref<array>([1])
+
+const addSighting = () => {
+  console.log(this.count)
+  this.count.push(this.count.length + 1)
+  console.log(this.count)
+}
+</script> -->
