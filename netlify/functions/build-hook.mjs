0bb2ff90b500e0743e83475df1768cba76873c21
Izzie <izzieccampbell@gmail.com>
2024-06-29 23:22:48 -0400

    Add daily build hooks

---
+++ netlify/functions/build-hook.mjs
@@ -0,0 +1,25 @@
+// An asynchronous function to call
+// the Netlify build hook to rebuild your site
+const rebuildSite = async triggerTitle => {
+    // Construct the URL for the Netlify rebuild hook
+    const url = new URL(process.env.BUILD_HOOK)
+  
+    // Add the title to the query string
+    url.searchParams.append("trigger_title", triggerTitle)
+  
+    // Make a POST request to the Netlify webhook
+    return await fetch(url.toString(), {
+      method: "POST"
+    })
+  }
+  
+  // Always update your footer every year! :)
+  export default async request => {
+    await rebuildSite("daily rebuild to update footer")
+  }
+  
+  // Netlify scheduled function cron syntax
+  // Run every day
+  export const config = {
+    schedule: "@daily"
+  }
