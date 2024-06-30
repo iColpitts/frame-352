588ad12a2ac7d1c53979e1b7e3f2cd78badc37ce
Izzie <izzieccampbell@gmail.com>
2024-06-29 21:48:52 -0400

    Display findings and format website.

---
+++ src/components/sightings.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="sightings-wrapper">
+    <div v-for="sighting in sightings" class="sighting">
+      <img :src="sighting.file.url"/>
+      <p class="subtitle">{{ sighting.date }}</p>
+      <p>{{ sighting.note }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+  name: "Sightings",
+  props: {
+    sightings: {
+      type: Array,
+      default: [],
+    },
+  },
+}
+</script>
+
+<style>
+img{
+  max-width:300px;
+  width: 100%;
+}
+
+.subtitle {
+  color: grey;
+  font-size: 10px;
+  padding-top: 0;
+}
+
+.text {
+  padding: 2px;
+  padding-top: 0px;
+  margin-top: 0px;
+}
+
+.sightings-wrapper {
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.sighting {
+  outline: 1px solid black;
+  padding: 2px;
+  max-width: 250px;
+}
+</style>
